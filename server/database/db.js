5b5c2e03f6b4e49b128d38a3a4dcaa0dd044ee27
Ryan Nelson <rnelson91295@gmail.com>
2022-09-27 13:05:18 -0400

    Wrote initial queries for yt api

--- server/database/db.js
+++ server/database/db.js
@@ -5,9 +5,12 @@ mongoose.connect('mongodb://localhost:27017/comments');
 console.log('MongoDB connected');
 
 const commentSchema = new mongoose.Schema({
+  commentId: String,
   username: String,
+  userId: String,
   text: String,
-  url: String,
+  likeCount: Number,
+  videoId: String,
   search: String
 });
 
--- server/routes.js
+++ server/routes.js
@@ -36,29 +36,67 @@ router.get('/comments', (req, res) => {
 // From that list of results, get the top comments
 // Save the top comments by url and search query
 // Results will be returned by search query
+// TODO: Add language filter on comment results
 router.post('/comments', (req, res) => {
   // console.log(req.body);
-  // axios.get(`${process.env.API_URL}`, {
-  //   headers: {
-  //     AUTH: process.env.API_AUTH
-  //   }
-  // })
-  model.saveComments(
-    [ // DEBUG - Using test data
-      {
-        username: 'ryan',
-        text: 'hello',
-        url: 'test.com',
-        search: req.body.search
-      },
-      {
-        username: 'ryan',
-        text: 'goodbye',
-        url: 'test.net',
-        search: req.body.search
+  // Search API for videos by query
+  axios.get(`${process.env.API_URL}/search`, {
+    params: {
+      part: [
+        'id'
+      ],
+      maxResults: 25,
+      order: 'relevance',
+      q: req.body.search
+    },
+    headers: {
+      Authorization: `Bearer ${process.env.API_AUTH}`,
+      Accept: 'application/json'
+    }
+  })
+    .then((results) => {
+      // Get videos from yt API by search query
+      let commentPromises = [];
+      for (let item of results.items) {
+        commentPromises.push(
+          axios.get(`${process.env.API_URL}/commentThreads`, {
+            params: {
+              part: [
+                'snippet'
+              ],
+              moderationStatus: 'published',
+              order: 'relevance',
+              searchTerms: req.body.search, // TODO: May take this out, get all comments for videos relevant to the query
+              videoId: item.id.videoId
+            },
+            headers: {
+              Authorization: `Bearer ${process.env.API_AUTH}`,
+              Accept: 'application/json'
+            }
+          })
+        );
       }
-    ]
-  )
+      return Promise.all(commentPromises);
+    })
+    .then((promiseResults) => {
+      let comments = [];
+      for (let commentResults of promiseResults) {
+        for (let item of commentResults.items) {
+          comments.push(
+            {
+              commentId: item.id,
+              username: item.snippet.topLevelComment.snippet.authorDisplayName,
+              userId: item.snippet.topLevelComment.snippet.authorChannelId.value,
+              text: item.snippet.topLevelComment.snippet.textOriginal,
+              likeCount: item.snippet.topLevelComment.snippet.likeCount,
+              videoId: item.snippet.topLevelComment.snippet.videoId,
+              search: req.body.search
+            }
+          )
+        }
+      }
+      return model.saveComments(comments);
+    })
     .then((results) => {
       console.log(results);
       res.sendStatus(201);
