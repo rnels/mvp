40d5c64275e0be260f7279360277ba96eead1df0
Ryan Nelson <rnelson91295@gmail.com>
2022-09-27 11:04:01 -0400

    Initial commit

---
+++ server/routes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/words', (req, res) => {
+  axios.get()
+});
+
+module.exports = router;
