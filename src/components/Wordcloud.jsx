e215ba638e6d1a401ef9b183489007251001af53
Ryan Nelson <rnelson91295@gmail.com>
2022-09-28 13:41:36 -0400

    Using stopword library

--- src/components/Wordcloud.jsx
+++ src/components/Wordcloud.jsx
@@ -1,6 +1,7 @@
 import ReactWordcloud from 'react-wordcloud';
-
 import { memo } from 'react';
+
+import { removeStopwords } from 'stopword';
 
 export default memo(function Wordcloud({comments, filter, minWords, maxWords, minScore, maxScore, commonWordFilter}) {
 
@@ -13,101 +14,14 @@ export default memo(function Wordcloud({comments, filter, minWords, maxWords, mi
   }
 
   let temp = {};
-  // Forgive me
-  let regex = [
-    'the',
-    'of',
-    'it',
-    'ive',
-    'then',
-    'than',
-    'only',
-    'most',
-    'a',
-    'an',
-    'i',
-    'im',
-    'as',
-    'we',
-    'she',
-    'he',
-    'her',
-    'hers',
-    'his',
-    'him',
-    'there',
-    'they',
-    'their',
-    'theirs',
-    'them',
-    'our',
-    'your',
-    'and',
-    'in',
-    'so',
-    'be',
-    'to',
-    'too',
-    'is',
-    'its',
-    'are',
-    'was',
-    'at',
-    'that',
-    'thats',
-    'this',
-    'for',
-    'by',
-    'any',
-    'just',
-    'or',
-    'has',
-    'had',
-    'but',
-    'like',
-    'do',
-    'does',
-    'on',
-    'you',
-    'if',
-    'also',
-    'can',
-    'have',
-    'get',
-    'with',
-    'from',
-    'some',
-    'all',
-    'way',
-    'think',
-    'were',
-    'go',
-    'who',
-    'lets',
-    'would',
-    'every',
-    'put',
-    'into',
-    'these'
-  ]
+
   for (let i = 0; i < comments.length; i++) {
     let split = comments[i].toLowerCase()
     .replace(/[.,/#!$%^&*;:{}=\-_`"'’~()]/g, "")
     .replace(/[\W_]+/g," ");
-    if (commonWordFilter) {
-      for (let r of regex) {
-        split = split.replace(new RegExp("\\b" + r + "\\b", 'g'), "");
-        split = split.replace(new RegExp("\\b" + r + "\\s", 'g'), "");
-        split = split.replace(new RegExp("\\s" + r + "\\b", 'g'), "");
-      }
-    }
     split = split.split(' ');
-    for (let i = 0; i < split.length; i++) {
-      split[i] = split[i].trim();
-      if (!split[i].length) {
-        split.splice(i, 1);
-        i--;
-      }
+    if (commonWordFilter) {
+      split = removeStopwords(split);
     }
     for (let z = minWords; z <= maxWords && z <= split.length; z++) {
       for (let j = 0; j + z <= split.length; j++) {
